70779f5fca6247cbeed19d610dcd43c8528adba9
darenang44 <darenang44@gmail.com>
2016-12-17 11:09:31 -0800

    Add movie to app index and show actions and view pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,11 @@
 class MoviesController < ApplicationController
+  before_action :find_movie,  only: [:show, :edit, :update, :destroy]
 
   def index
+    @movies = Movie.all.order('created_at DESC')
+  end
+
+  def show
   end
 
   def new
@@ -11,16 +16,32 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
 
     if @movie.save
-      redirect_to @movie
+      redirect_to root_path
     else
       render 'new'
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 
   private
 
   def movie_params
     params.require(:movie).permit(:title, :description, :director)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:id])
   end
 
 end
