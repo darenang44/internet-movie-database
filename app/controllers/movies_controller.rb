88de7908fc7813e78a57e60336d1528007a2a8ca
darenang44 <darenang44@gmail.com>
2016-12-17 10:42:35 -0800

    Add new and create action with veiw file

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,26 @@
 class MoviesController < ApplicationController
 
-  def index  
+  def index
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to @movie
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :director)
   end
 
 end
