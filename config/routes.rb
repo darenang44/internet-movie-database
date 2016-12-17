97502fe29c7751d128589d3018a591f01a198d38
darenang44 <darenang44@gmail.com>
2016-12-17 10:15:22 -0800

    Add movies model,veiw, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :movies
+  root 'movies#index'
 end
+
+# Prefix Verb   URI Pattern                Controller#Action
+#     movies GET    /movies(.:format)          movies#index
+#            POST   /movies(.:format)          movies#create
+#  new_movie GET    /movies/new(.:format)      movies#new
+# edit_movie GET    /movies/:id/edit(.:format) movies#edit
+#      movie GET    /movies/:id(.:format)      movies#show
+#            PATCH  /movies/:id(.:format)      movies#update
+#            PUT    /movies/:id(.:format)      movies#update
+#            DELETE /movies/:id(.:format)      movies#destroy
+#       root GET    /                          movies#index
